80c4f23adb2c61f194c4db432e36957bf8a8ed92
Jeremy Miller <jeremy@dischan.org>
2016-04-17 19:06:52 -0400

    Can now run dismae projects with dismae as an external module.

---
+++ app/js/app.js
@@ -0,0 +1,39 @@
+var process = require('process');
+var ipc = require('electron').ipcRenderer;
+const dialog = require('electron').remote.dialog;
+var dismae = require('/Users/jeremy/Git/dismae');
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    projects: []
+  },
+  methods: {
+    updateProjectList: function updateProjectList(projects) {
+      this.projects = projects;
+    },
+    addProject: function addProject(paths) {
+      if(paths){
+        ipc.send('add-project', paths[0]);
+      }
+    },
+    startProject: function startProject(path) {
+      var config = require(path + '/config');
+      config.gameDir = path;
+      var game = dismae(config);
+      game.start();
+    },
+    addProjectDialog: function addProjectDialog() {
+      dialog.showOpenDialog({
+          title: 'Choose Project Directory',
+          properties: ['openDirectory', 'createDirectory']
+        },
+        this.addProject
+      );
+    }
+  }
+});
+
+ipc.on('config-loaded', function(event, config) {
+  app.updateProjectList(config.projects);
+});
---
+++ app/main.js
@@ -0,0 +1,71 @@
+var electron = require('electron');
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var ipc = require("electron").ipcMain;
+var fs = require("fs");
+
+var config = {projects: []};
+var userData = app.getPath("userData");
+var configFile = app.getPath("userData") + '/config.json';
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+
+function writeConfig(config) {
+  fs.writeFileSync(configFile, JSON.stringify(config), 'utf8');
+}
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+ipc.on('add-project', function(event, arg) {
+  config.projects.push(arg);
+  writeConfig(config);
+});
+
+app.on('ready', function() {
+  //get screen info
+  var electronScreen = electron.screen;
+  var size = electronScreen.getPrimaryDisplay().bounds;
+  var windowSize = {};
+
+  windowSize.width = 1024;
+  windowSize.height = 576;
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: windowSize.width, height: windowSize.height, x:0, y:0, resizable: true, useContentSize: true});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.focus();
+
+  //load config file
+  try {
+    stats = fs.lstatSync(userData);
+  } catch (e) {
+    fs.mkdirSync(userData);
+  }
+  try {
+    stats = fs.lstatSync(configFile);
+    config = require(configFile);
+  } catch (e) {}
+
+  mainWindow.webContents.on('did-finish-load', function() {
+    mainWindow.webContents.send('config-loaded', config);
+  });
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
