77f5907edd018a3733c8be8338c53a5984607ea2
Jeremy Miller <jeremy@dischan.org>
2016-04-20 20:40:33 -0400

    Display status during project start/build.

--- app/js/app.js
+++ app/js/app.js
@@ -7,7 +7,8 @@ var config;
 var app = new Vue({
   el: '#app',
   data: {
-    projects: []
+    projects: [],
+    status: null
   },
   methods: {
     updateProjectList: function updateProjectList(projects) {
@@ -27,9 +28,35 @@ var app = new Vue({
       ipc.send('update-config', config);
     },
     startProject: function startProject(path) {
+      var _this = this;
       var config = require(path + '/config');
       config.gameDir = path;
-      var game = dismae(config);
+      var game = new dismae(config);
+      game.on('update', (event) => {
+        switch(event) {
+          case 'checking dependencies':
+            _this.status = 'Checking dependencies...';
+          break;
+          case 'downloading dependencies':
+            _this.status = 'Downloading dependencies...';
+          break;
+          case 'extracting files':
+            _this.status = 'Extracting files...';
+          break;
+          case 'cleaning':
+            _this.status = 'Removing old build...';
+          break;
+          case 'building':
+            _this.status = 'Building...';
+          break;
+          case 'starting':
+            _this.status = 'Starting';
+          break;
+          case 'closed':
+            _this.status = null;
+          break;
+        }
+      });
       game.start();
     },
     addProjectDialog: function addProjectDialog() {
