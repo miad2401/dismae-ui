c2e13a1aa1cef24283a521ccdabd30355c550162
Jeremy Miller <jeremy@dischan.org>
2016-04-24 14:10:28 -0400

    Standardjs compliance.

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,11 @@
-var process = require('process');
-var ipc = require('electron').ipcRenderer;
-const dialog = require('electron').remote.dialog;
-var dismae = require('dismae');
-var config;
-var fs = require("fs");
-var path = require('path');
-var Download = require('download');
+var ipc = require('electron').ipcRenderer
+const dialog = require('electron').remote.dialog
+var Dismae = require('dismae')
+var config
+var fs = require('fs')
+var Download = require('download')
 
-var app = new Vue({
+var app = new window.Vue({
   el: '#app',
   data: {
     projects: [],
@@ -15,116 +13,116 @@ var app = new Vue({
     uiVersion: null
   },
   methods: {
-    updateProjectList: function updateProjectList(projects) {
-      this.projects = projects;
+    updateProjectList: function updateProjectList (projects) {
+      this.projects = projects
     },
-    updateUIVersion: function updateUIVersion(uiVersion) {
-      console.log(uiVersion);
-      this.uiVersion = uiVersion;
+    updateUIVersion: function updateUIVersion (uiVersion) {
+      console.log(uiVersion)
+      this.uiVersion = uiVersion
     },
-    addProject: function addProject(paths) {
-      if(paths){
-        config.projects.push(paths[0]);
-        this.projects = config.projects;
+    addProject: function addProject (paths) {
+      if (paths) {
+        config.projects.push(paths[0])
+        this.projects = config.projects
 
-        ipc.send('update-config', config);
+        ipc.send('update-config', config)
       }
     },
-    createProject: function createProject(paths) {
-      var _this = this;
-      if(paths){
-        fs.readdir(paths[0], function(err, files){
-          if(err){
-            console.log(err);
-            //throw error
-          } else {
-            function isHiddenFile(file) {
-              return !file.startsWith('.');
-            }
-            var nonHiddenFiles = files.filter(isHiddenFile);
-            if(nonHiddenFiles.length > 0) {
-              console.log(files);
-              //throw error (must be empty directory)
-            } else {
-              _this.status = 'Downloading and extracting base project...';
-              var zipPath = path.join(paths[0], 'base.zip');
-              new Download({mode: '755', extract: true, strip: 1})
-              .get('https://github.com/Dischan/dismae-base/archive/master.zip')
-              .dest(paths[0])
-              .run(function(){
-                config.projects.push(paths[0]);
-                this.projects = config.projects;
+    createProject: function createProject (paths) {
+      function isHiddenFile (file) {
+        return !file.startsWith('.')
+      }
 
-                ipc.send('update-config', config);
-                _this.status = null;
-              });
+      var _this = this
+      if (paths) {
+        fs.readdir(paths[0], function (err, files) {
+          if (err) {
+            console.log(err)
+          // throw error
+          } else {
+            var nonHiddenFiles = files.filter(isHiddenFile)
+            if (nonHiddenFiles.length > 0) {
+              console.log(files)
+            // throw error (must be empty directory)
+            } else {
+              _this.status = 'Downloading and extracting base project...'
+              new Download({mode: '755', extract: true, strip: 1})
+                .get('https://github.com/Dischan/dismae-base/archive/master.zip')
+                .dest(paths[0])
+                .run(function () {
+                  config.projects.push(paths[0])
+                  this.projects = config.projects
+
+                  ipc.send('update-config', config)
+                  _this.status = null
+                })
             }
           }
         })
       }
     },
-    removeProject: function removeProject(index) {
-      config.projects.splice(index, 1);
-      this.projects = config.projects;
-      ipc.send('update-config', config);
+    removeProject: function removeProject (index) {
+      config.projects.splice(index, 1)
+      this.projects = config.projects
+      ipc.send('update-config', config)
     },
-    startProject: function startProject(path) {
-      var _this = this;
-      var config = require(path + '/config');
-      config.gameDir = path;
-      var game = new dismae(config);
+    startProject: function startProject (path) {
+      var _this = this
+      var config = require(path + '/config')
+      config.gameDir = path
+      var game = new Dismae(config)
       game.on('update', (event) => {
-        switch(event) {
+        switch (event) {
           case 'checking dependencies':
-            _this.status = 'Checking dependencies...';
-          break;
+            _this.status = 'Checking dependencies...'
+            break
           case 'downloading dependencies':
-            _this.status = 'Downloading dependencies...';
-          break;
+            _this.status = 'Downloading dependencies...'
+            break
           case 'extracting files':
-            _this.status = 'Extracting files...';
-          break;
+            _this.status = 'Extracting files...'
+            break
           case 'cleaning':
-            _this.status = 'Removing old build...';
-          break;
+            _this.status = 'Removing old build...'
+            break
           case 'building':
-            _this.status = 'Building...';
-          break;
+            _this.status = 'Building...'
+            break
           case 'starting':
-            _this.status = 'Starting';
-          break;
+            _this.status = 'Starting'
+            break
           case 'closed':
-            _this.status = null;
-          break;
+            _this.status = null
+            break
         }
-      });
-      game.start();
+      })
+      game.start()
     },
-    addProjectDialog: function addProjectDialog() {
+    addProjectDialog: function addProjectDialog () {
       dialog.showOpenDialog({
-          title: 'Choose Project Directory',
-          properties: ['openDirectory', 'createDirectory']
-        },
+        title: 'Choose Project Directory',
+        properties: ['openDirectory', 'createDirectory']
+      },
         this.addProject
-      );
+      )
     },
-    createProjectDialog: function createProjectDialog() {
+    createProjectDialog: function createProjectDialog () {
       dialog.showOpenDialog({
-          title: 'Choose Project Directory',
-          properties: ['openDirectory', 'createDirectory']
-        },
+        title: 'Choose Project Directory',
+        properties: ['openDirectory', 'createDirectory']
+      },
         this.createProject
-      );
+      )
     }
   }
-});
+})
 
-ipc.on('config-loaded', function(event, conf) {
-  config = conf;
-  app.updateProjectList(config.projects);
-  app.updateUIVersion(config.uiVersion);
-});
+ipc.on('config-loaded', function (event, conf) {
+  config = conf
+  app.updateProjectList(config.projects)
+  app.updateUIVersion(config.uiVersion)
+})
 
-ipc.on('updater', function(event, msg) {
-  console.log(msg);
-});
+ipc.on('updater', function (event, msg) {
+  console.log(msg)
+})
--- app/main.js
+++ app/main.js
@@ -1,115 +1,113 @@
-if (require('electron-squirrel-startup')) return;
-var electron = require('electron');
-var autoUpdater = require('electron').autoUpdater;
-var app = require('app');
-var BrowserWindow = require('browser-window');
-var ipc = require("electron").ipcMain;
-var fs = require("fs");
-var config = {projects: []};
-var userData = app.getPath("userData");
-var configFile = app.getPath("userData") + '/config.json';
-var os = require("os");
+if (require('electron-squirrel-startup')) {
+  require('process').exit()
+}
+var autoUpdater = require('electron').autoUpdater
+var app = require('app')
+var BrowserWindow = require('browser-window')
+var ipc = require('electron').ipcMain
+var fs = require('fs')
+var config = {projects: []}
+var userData = app.getPath('userData')
+var configFile = app.getPath('userData') + '/config.json'
+var os = require('os')
 
-const UPDATE_SERVER_HOST = "dismae-update-server.herokuapp.com"
+const UPDATE_SERVER_HOST = 'dismae-update-server.herokuapp.com'
 
-function checkUpdates() {
-  var platform = os.platform();
-  var url;
-  const version = app.getVersion();
+function checkUpdates () {
+  var platform = os.platform()
+  var url
+  const version = app.getVersion()
 
-  if (platform === "linux") {
-    return;
-  } else if (platform === "darwin") {
+  if (platform === 'linux') {
+    return
+  } else if (platform === 'darwin') {
     url = `https://${UPDATE_SERVER_HOST}/update/darwin_x64/${version}`
-  } else if (platform === "win32") {
+  } else if (platform === 'win32') {
     url = `https://${UPDATE_SERVER_HOST}/update/win32/${version}`
   }
 
-  autoUpdater.addListener("update-available", function (event) {
-    mainWindow.webContents.send('updater', "A new update is available");
-    console.log("A new update is available")
+  autoUpdater.addListener('update-available', function (event) {
+    mainWindow.webContents.send('updater', 'A new update is available')
+    console.log('A new update is available')
   })
-  autoUpdater.addListener("update-downloaded", function (event, releaseNotes, releaseName, releaseDate, updateURL) {
-    mainWindow.webContents.send('updater', "A new update is ready to install" + `Version ${releaseName} is downloaded and will be automatically installed on Quit`);
-    console.log("A new update is ready to install", `Version ${releaseName} is downloaded and will be automatically installed on Quit`)
+  autoUpdater.addListener('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateURL) {
+    mainWindow.webContents.send('updater', 'A new update is ready to install' + `Version ${releaseName} is downloaded and will be automatically installed on Quit`)
+    console.log('A new update is ready to install', `Version ${releaseName} is downloaded and will be automatically installed on Quit`)
   })
-  autoUpdater.addListener("error", function (event, message) {
-    mainWindow.webContents.send('updater', message);
+  autoUpdater.addListener('error', function (event, message) {
+    mainWindow.webContents.send('updater', message)
     console.log(message)
   })
-  autoUpdater.addListener("checking-for-update", function (event) {
-    mainWindow.webContents.send('updater', "checking-for-update");
-    console.log("checking-for-update")
+  autoUpdater.addListener('checking-for-update', function (event) {
+    mainWindow.webContents.send('updater', 'checking-for-update')
+    console.log('checking-for-update')
   })
-  autoUpdater.addListener("update-not-available", function () {
-    mainWindow.webContents.send('updater', "update-not-available");
-    console.log("update-not-available")
+  autoUpdater.addListener('update-not-available', function () {
+    mainWindow.webContents.send('updater', 'update-not-available')
+    console.log('update-not-available')
   })
-  mainWindow.webContents.send('updater', "setting feed url to: " + url);
+  mainWindow.webContents.send('updater', 'setting feed url to: ' + url)
   autoUpdater.setFeedURL(url)
   autoUpdater.checkForUpdates()
 }
 
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+var mainWindow = null
 
-function writeConfig(config) {
-  fs.writeFileSync(configFile, JSON.stringify(config), 'utf8');
+function writeConfig (config) {
+  fs.writeFileSync(configFile, JSON.stringify(config), 'utf8')
 }
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  app.quit();
-});
+app.on('window-all-closed', function () {
+  app.quit()
+})
 
-ipc.on('update-config', function(event, conf) {
-  writeConfig(conf);
-});
+ipc.on('update-config', function (event, conf) {
+  writeConfig(conf)
+})
 
-app.on('ready', function() {
-  //get screen info
-  var electronScreen = electron.screen;
-  var size = electronScreen.getPrimaryDisplay().bounds;
-  var windowSize = {};
+app.on('ready', function () {
+  // get screen info
+  var windowSize = {}
 
-  windowSize.width = 1024;
-  windowSize.height = 576;
+  windowSize.width = 1024
+  windowSize.height = 576
 
   // Create the browser window.
-  mainWindow = new BrowserWindow({title: 'Dismae ' + app.getVersion(), width: windowSize.width, height: windowSize.height, x:0, y:0, resizable: true, useContentSize: true});
+  mainWindow = new BrowserWindow({title: 'Dismae ' + app.getVersion(), width: windowSize.width, height: windowSize.height, x: 0, y: 0, resizable: true, useContentSize: true})
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html')  // eslint-disable-line no-path-concat
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools()
 
-  mainWindow.focus();
+  mainWindow.focus()
 
-  //load config file
+  // load config file
   try {
-    stats = fs.lstatSync(userData);
+    fs.lstatSync(userData)
   } catch (e) {
-    fs.mkdirSync(userData);
+    fs.mkdirSync(userData)
   }
   try {
-    stats = fs.lstatSync(configFile);
-    config = require(configFile);
+    fs.lstatSync(configFile)
+    config = require(configFile)
   } catch (e) {}
 
-  mainWindow.webContents.on('did-finish-load', function() {
-    config.uiVersion = app.getVersion();
-    mainWindow.webContents.send('config-loaded', config);
-    checkUpdates(mainWindow);
-  });
+  mainWindow.webContents.on('did-finish-load', function () {
+    config.uiVersion = app.getVersion()
+    mainWindow.webContents.send('config-loaded', config)
+    checkUpdates(mainWindow)
+  })
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-});
+    mainWindow = null
+  })
+})
